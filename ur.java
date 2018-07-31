1fbfe275e89eec72f35d63f1f11fef5304926cd7
Nicholas Cockcroft <35208141+ncockcro@users.noreply.github.com>
2018-07-31 10:16:38 -0400

    Generating recommendations for users

--- ur.java
+++ ur.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 import java.lang.*;
 import java.util.*;
-import java.io.*;
+import java.io.*; 
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
